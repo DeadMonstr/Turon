8e042f4dcf37179540dc4f26910f532003d50b29
DeadMonstr <deedmonstr@gmail.com>
2024-06-08 17:13:55 +0500

    changed gitignore

--- src/pages/website/Website.js
+++ src/pages/website/Website.js
@@ -3,7 +3,7 @@ import {Home} from "components/website";
 const Website = () => {
     return (
         <div>
-            <h1>\hello</h1>
+            <h1>\hello shaha</h1>
             <Home/>
         </div>
     );
