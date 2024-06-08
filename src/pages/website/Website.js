2c52f368612100001e592585385f9f5e9494187b
ikromovvv <isardor859@gmail.com>
2024-06-08 17:27:37 +0500

    sarik

--- src/pages/website/Website.js
+++ src/pages/website/Website.js
@@ -3,6 +3,7 @@ import {Home} from "components/website";
 const Website = () => {
     return (
         <div>
+            <h1>hello sarik</h1>
             <h1>\hello shaha</h1>
             <Home/>
         </div>
